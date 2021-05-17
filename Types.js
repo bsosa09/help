4c239b9dd684660873a219ddb8688786cb7a21af
bsosa09 <55119813+bsosa09@users.noreply.github.com>
2021-05-16 22:55:54 -0700

    Add files via upload

---
+++ Types.js
@@ -0,0 +1,13 @@
+const Types = {
+    setShowMenu:'SET_SHOW_MENU',
+    setShowMenuManual:'SET_SHOW_MENU_MANUAL',
+    setAuth:'SET_AUTH',
+    setCurrentName:'SET_CURRENT_NAME',
+    setUser:'SET_USER',
+    setDocs:'SET_DOCS',
+    setToolbarDisplay:'SET_TOOLBAR_DISPLAY',
+    setPreferences:'SET_PREFERENCES',
+    setShowModal:'SET_SHOW_MODAL',
+    setModalContent:'SET_MODAL_CONTENT'
+ }
+ export default Types;
